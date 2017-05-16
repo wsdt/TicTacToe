5314ba3c2fa61d11525fa85ac43ee7e3718ee3e0
Riedl Kevin Alfred <1610653212@fh-kufstein.ac.at>
2017-05-16 10:24:29 +0200

    BatFile Warning added

--- pushwholeproject.bat
+++ pushwholeproject.bat
@@ -3,6 +3,13 @@ color c
 echo.
 echo *********************** Push everything ***********************
 echo.
+echo WARNING: ALL FILES ARE GOING TO BE OVERWRITTEN!!!
+echo.
+echo git add .
+echo git commit -m [your message]
+echo git push
+
+echo.
 set message=%date%_%username%
 set /p "message=Your Commit-Message: "
 
