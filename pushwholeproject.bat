5e66c46b7973788fc9207f9f82ee269bfd89745e
Riedl Kevin Alfred <1610653212@fh-kufstein.ac.at>
2017-05-16 10:32:22 +0200

    BatFile enhanced

--- pushwholeproject.bat
+++ pushwholeproject.bat
@@ -3,17 +3,22 @@ color c
 echo.
 echo *********************** Push everything ***********************
 echo.
-echo WARNING: ALL FILES ARE GOING TO BE OVERWRITTEN!!!
-echo.
+
+
 echo git add .
 echo git commit -m [your message]
 echo git push
 
 echo.
+set files=.
+set /p "files=File to be added [. for whole project]: "
+
+if "%files%" equ "." echo WARNING: ALL FILES ARE GOING TO BE OVERWRITTEN!!!
+echo.
 set message=%date%_%username%
 set /p "message=Your Commit-Message: "
 
-git add .
+git add %files%
 git commit -m "%message%"
 git push
 
