d48c4f2e02d4c8d5d56e574ba72fc6713ad1a0d5
Riedl Kevin Alfred <1610653212@fh-kufstein.ac.at>
2017-06-20 14:40:02 +0200

    added js form login

--- php/functions.php
+++ php/functions.php
@@ -17,10 +17,10 @@ function createLoginForm() {
         <div class=\"modal-dialog\">
             <div class=\"loginmodal-container\">
                 <h1>Login</h1><br>
-                <form method=\"post\" action=\"\">
-                    <input type=\"text\" name=\"username\" placeholder=\"Username\">
-                    <input type=\"password\" name=\"passwort\" placeholder=\"Passwort\">
-                    <input type=\"submit\" name=\"login\" class=\"login loginmodal-submit\" value=\"Login\" onclick='validateLoginCredentials()'>
+                <form method=\"post\" action=\"\" onsubmit='return validateLoginCredentials()'>
+                    <input type=\"text\" name=\"username\" placeholder=\"Username\" id='log_username' onfocus='close_notification()'>
+                    <input type=\"password\" name=\"passwort\" placeholder=\"Passwort\" id='log_password' onfocus='close_notification()'>
+                    <input type=\"submit\" name=\"login\" class=\"login loginmodal-submit\" value=\"Login\">
                 </form>
 
                 <div class=\"login-help\">
