0ea3581205d779a097b78776aab3ad52541d758a
kevin <kevin.riedl.privat@gmail.com>
2017-06-22 12:32:21 +0200

    head changed

--- index.php
+++ index.php
@@ -5,7 +5,20 @@
 <head>
     <title>TicTacToe</title>
 <?php
-        require_once('php/head.php');
+        //require_once('php/head.php');
+
+ echo "<meta charset=\"utf-8\">
+    <link href=\"bootstrap/docs/dist/css/bootstrap.min.css\" rel=\"stylesheet\"/>
+    <link href=\"bootstrap/docs/dist/css/bootstrap-theme.min.css\" rel=\"stylesheet\"/>
+    <link href=\"bootstrap/bootstrap-social-gh-pages/bootstrap-social.css\" rel=\"stylesheet\"/>
+    <link href=\"css/tictactoe.css\" rel=\"stylesheet\">
+    <link rel='icon' href='images/ttt_icon_hp.ico'>
+    <script type=\"text/javascript\" src=\"js/tictactoe.js\"></script>
+    <script type=\"text/javascript\" src=\"jquery/jquery-3.2.1.min.js\"></script>
+    <script type=\"text/javascript\" src=\"bootstrap/dist/js/bootstrap.min.js\"></script>
+    <script type=\"text/javascript\" src=\"js/ttt_gamelogic.js\"></script>
+    <script type='text/javascript' src='js/login_register.js'></script>";
+    require_once 'php/functions.php';
     ?>
 
 </head>
